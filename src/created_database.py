b55898cbb8a86dc8bf2274bcf9494168c157e369
Juan De la Rosa <juan.d@futurelinkcorporation.com>
2018-02-21 16:24:54 -0400

    add script for the initialitation of the database

---
+++ src/created_database.py
@@ -0,0 +1,16 @@
+import db_modules
+
+categories=[
+    'Displays',
+    'Computer parts',
+    'Accessories',
+    'Laptops',
+    'Software'
+]
+
+
+if __name__ == "__main__":
+    db_modules.Base.metadata.create_all(db_modules.engine)
+    for category in categories :
+        data={'name':category}
+        db_modules.createCategory(data)
